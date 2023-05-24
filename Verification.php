6cff9085f0f1c6920279adb64dd8fa73c277cd18
DevRubia <78357146+DevRubia@users.noreply.github.com>
2023-05-24 11:12:53 +0300

    Create Verification.php

---
+++ Verification.php
@@ -0,0 +1,14 @@
+//Usage
+//Open the file where you want to perform email verification, and include the necessary Firebase PHP SDK classes:
+
+<?php
+use Kreait\Firebase\Auth;
+//Initialize the Firebase Auth instance:
+
+// Assuming you have already initialized the Firebase Auth instance
+$auth = new Auth();
+Generate an email verification link:
+
+// Provide the user's email
+$link = $auth->getEmailVerificationLink($email);
+?>
